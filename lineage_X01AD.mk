9e9bce18c7207dcee77d513b1b059f94f68bff9c
AP-XD <arkamtg.pramanik@gmail.com>
2021-03-12 20:28:45 +0530

    X01AD: Initial Adaptation for sakura

--- lineage_X01AD.mk
+++ lineage_X01AD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The PixelExperience Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,21 +13,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit common Pixelexperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common LineageOS stuff.
 TARGET_BOOT_ANIMATION_RES := 720
-TARGET_GAPPS_ARCH := arm64
-
-# Pixel customization
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device stuff
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
+# Sakura flags
+SAKURA_OFFICIAL := true
+SAKURA_MAINTAINER := KryptoniteX&AP_XD
 # Device identifiers.
 PRODUCT_DEVICE := X01AD
-PRODUCT_NAME := aosp_X01AD
+PRODUCT_NAME := lineage_X01AD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01AD
 PRODUCT_MANUFACTURER := asus
